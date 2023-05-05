de7304de984fba43b80091941b5b5398ca494119
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-05-04 22:30:32 -0400

    Finished working on Clock project for the day pushing up to Github

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Display the hours, minutes, and seconds.
+-The clock should run by itself. (Hint: look at JavaScript setInterval function)
+-Google "javascript date get time" to see the built-in JavaScript helpers you can use.
+-The clock should automatically update the time every second.
+*/
+
+let hour = document.querySelector('.hour');
+let minute = document.querySelector('.minute');
+let second = document.querySelector('second');
+
+
+
