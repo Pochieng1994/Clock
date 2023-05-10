9f6abc028e8a12982f6f666d868c3359384b8ee8
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-05-10 14:06:27 -0400

    Finished working on Clock pushing up to Github

--- script.js
+++ script.js
@@ -6,7 +6,22 @@
 
 let hour = document.querySelector('.hour');
 let minute = document.querySelector('.minute');
-let second = document.querySelector('second');
+let second = document.querySelector('.second');
+
+
+setInterval(function() {
+  let currentDate = new Date();
+
+  let hours = currentDate.getHours();
+  hour.textContent = hours + ':';
+
+  let minutes = currentDate.getMinutes();
+  minute.textContent = minutes + ':';
+
+  let seconds = currentDate.getSeconds();
+  second.textContent = seconds;
+},1000);
+
 
 
 
