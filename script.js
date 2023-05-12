670df44e795b16ba91d39fd09d795e92a029ba78
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-05-12 00:29:17 -0400

    Finished working on Clock project pushing up to github

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let hour = document.querySelector('.hour');
 let minute = document.querySelector('.minute');
 let second = document.querySelector('.second');
+let amPM = document.querySelector('.am-pm');
 
 
 setInterval(function() {
   let currentDate = new Date();
 
-  let hours = currentDate.getHours();
+  let hours = currentDate.getHours() > 12 ? currentDate.getHours() - 12 : currentDate.getHours();
   hour.textContent = hours + ':';
+
+  let morningOrAfternoon = currentDate.getHours() >= 12 ? 'PM' : 'AM';
+  amPM.textContent = morningOrAfternoon 
 
   let minutes = currentDate.getMinutes();
   minute.textContent = minutes + ':';
@@ -15,7 +19,6 @@ setInterval(function() {
   let seconds = currentDate.getSeconds();
   second.textContent = seconds;
 },1000);
-
 
 
 
